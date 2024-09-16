758a2f17ff302c2f0153f7cba521a2a887e00fa5
gusmini <enricogusmini26@gmail.com>
2024-09-16 12:42:46 +0200

    menu

--- src/ripasso.java
+++ src/ripasso.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ripasso {
     public static void main(String[] args) {
+        System.out.println("\nMenu:");
+        System.out.println("1. Aggiunta di una nuova auto");
+        System.out.println("2. Visualizzazione di tutte le auto");
+        System.out.println("3. Ricerca di un'auto tramite marca e modello");
+        System.out.println("4. Cancellazione di un'auto");
+        System.out.println("5. Modifica dei dati di un'auto");
+        System.out.println("6. Visualizzazione delle auto in ordine crescente");
+        System.out.println("7. Visualizzazione di tutti i modelli che diesel");
+        System.out.println("0. Esci");
+        System.out.print("Scegli un'opzione: ");
 
     }
 }
