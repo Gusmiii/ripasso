85286f1191420766f3e197076d723a831a033e47
gusmini <enricogusmini26@gmail.com>
2024-09-16 13:03:47 +0200

    cases

--- src/ripasso.java
+++ src/ripasso.java
@@ -1,7 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class ripasso {
     public static void main(String[] args) {
+        int scelta;
+        int macchina;
+        Scanner in = new Scanner(System.in);
         System.out.println("\nMenu:");
         System.out.println("1. Aggiunta di una nuova auto");
         System.out.println("2. Visualizzazione di tutte le auto");
@@ -12,6 +14,42 @@ public class ripasso {
         System.out.println("7. Visualizzazione di tutti i modelli che diesel");
         System.out.println("0. Esci");
         System.out.print("Scegli un'opzione: ");
+        scelta = in.nextInt();
+        switch (scelta) {
+            case 1:
+                System.out.println("Opzione 1: metti il nome dell auto che vuoi aggiungere");
+                macchina = in.nextInt();
+                break;
+            case 2:
+                System.out.println("Opzione 2: Visualizzazione di tutte le auto");
 
+                break;
+            case 3:
+                System.out.println("Opzione 3: Ricerca di un'auto tramite marca e modello");
+
+                break;
+            case 4:
+                System.out.println("Opzione 4: Cancellazione di un'auto");
+
+                break;
+            case 5:
+                System.out.println("Opzione 5: Modifica dei dati di un'auto");
+
+                break;
+            case 6:
+                System.out.println("Opzione 6: Visualizzazione delle auto in ordine crescente");
+                break;
+            case 7:
+                System.out.println("Opzione 7: Visualizzazione di tutti i modelli che diesel");
+
+                break;
+            case 0:
+                System.out.println("Uscita...");
+                return;
+            default:
+                System.out.println("Opzione non valida.")
+        }
     }
 }
+ //public static int aggiunta (int [] macchine){
+
