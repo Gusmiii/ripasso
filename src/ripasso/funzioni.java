df677d4a8c25de1b9520d9898f5b1c7dec5d91e9
gusmini <enricogusmini26@gmail.com>
2024-09-23 12:46:58 +0200

    funzione 2 e una parte della 3

--- src/ripasso/funzioni.java
+++ src/ripasso/funzioni.java
@@ -2,8 +2,26 @@ package ripasso;
 
 public class funzioni {
 
-    public static int aggiunta (String nomemacchina,int prezzo,int indice, String modello,String[] macchine, String[]nomemodello,String[]arraprezzo){
+    public static void aggiunta (String nomemacchina,double prezzo,int indice, String modello,String[] macchine, String[]modelloauto,double []arrayprezzo){
         macchine[indice]=nomemacchina;
+        modelloauto[indice]=modello;
+        arrayprezzo[indice]=prezzo;
     }
+    public static String visualizzazione (String[] macchine, String[]modelloauto,double []arrayprezzo){
+        String stringaes2 = "";
+        for (int i = 0;i< modelloauto.length;i++){
+          stringaes2 +=  ("nome macchina:"+macchine[i]+"modello:"+modelloauto[i]+"prezzo:"+arrayprezzo[i]);
+
+        }
+        return stringaes2;
+    }
+    public static void ricerca (String[] macchine, String[]modelloauto,String nomemacchina, String modello){
+        for (int i = 0; i < modelloauto.length;i++){
+            if(nomemacchina.equals(macchine[i] && modello.equals(modelloauto[i]))){
+
+            }
+        }
+    }
+
 }
 
--- src/ripasso/ripassoo.java
+++ src/ripasso/ripassoo.java
@@ -5,10 +5,10 @@ public class ripassoo {
         int scelta;
         String nomemacchina;
         String modello;
-        int prezzo = 0;
+        double prezzo = 0;
         String [] macchine = new String[100];
         String [] modelloauto = new String[100];
-        int [] arrayprezzo = new int [100];
+        double [] arrayprezzo = new double [100];
         int indice =0;
 
 
@@ -30,15 +30,19 @@ public class ripassoo {
                 nomemacchina = in.nextLine();
                 modello =in.nextLine();
                 prezzo = in.nextInt();
+                funzioni.aggiunta(nomemacchina,prezzo,indice,modello,macchine,modelloauto,arrayprezzo);
+                indice++;
 
                 break;
             case 2:
                 System.out.println("Opzione 2: Visualizzazione di tutte le auto");
-
+            funzioni.visualizzazione(macchine,modelloauto,arrayprezzo);
                 break;
             case 3:
                 System.out.println("Opzione 3: Ricerca di un'auto tramite marca e modello");
-
+            System.out.println("scrivi in odine la marca e il modello dell auto che ti interessa cercare");
+            nomemacchina=in.nextLine();
+            modello=in.nextLine();
                 break;
             case 4:
                 System.out.println("Opzione 4: Cancellazione di un'auto");
